cfcc227ef18896431811bb6d383c6bdcc0c338bc
nvtaikma <nvtai0202@gmail.com>
2023-11-07 22:52:33 +0700

    swr

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,33 @@ import style from "@/styles/app.module.css";
 import Link from "next/link";
 import Container from "react-bootstrap/Container";
 import TableExample from "../components/app.table";
+import { useEffect } from "react";
+import useSWR from "swr";
 
 export default function Home() {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const res = await fetch("http://localhost:8000/blogs");
+  //     const data = await res.json();
+  //     console.log(">> Fetch data", data);
+  //   };
+  //   fetchData();
+  // }, []);
+  const fetcher = (url: string) => fetch(url).then((res) => res.json());
+  const { data, error, isLoading } = useSWR(
+    "http://localhost:8000/blogs",
+    fetcher,
+    {
+      revalidateIfStale: false,
+      revalidateOnFocus: false,
+      revalidateOnReconnect: false,
+    }
+  );
+  console.log(">>> data", data);
+
   return (
     <div>
+      <div>{data?.length}</div>
       <ul>
         <li className={style["red"]}>
           <Link href="/facebook">Facebook</Link>
--- src/components/app.header.tsx
+++ src/components/app.header.tsx
@@ -1,4 +1,5 @@
 "use client";
+import Link from "next/link";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
@@ -8,23 +9,13 @@ function Header() {
   return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Link className="navbar-brand" href="/">React-Bootstrap</Link>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
-            <NavDropdown title="Dropdown" id="basic-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
+            <Link className="nav-link" href="/facebook">Facebook</Link>
+            <Link className="nav-link" href="/tiktok">TikTok</Link>
+            <Link className="nav-link" href="/youtube">Youtube</Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
